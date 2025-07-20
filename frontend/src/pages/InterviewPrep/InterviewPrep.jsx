ccb7a863b74fe525da81fc70e745463c029faf42
imsanthosh7 <154437536+imsanthosh7@users.noreply.github.com>
2025-07-20 19:51:44 +0530

    update

--- frontend/src/pages/InterviewPrep/InterviewPrep.jsx
+++ frontend/src/pages/InterviewPrep/InterviewPrep.jsx
@@ -9,6 +9,7 @@ import DashboardLayout from '../../components/layouts/DashboardLayout';
 import RoleInfoHeader from './components/RoleInfoHeader';
 import axios from 'axios';
 import { API_PATHS } from '../../utils/apipath.js';
+import QuestionCard from '../../components/Cards/QuestionCard.jsx';
 
 
 const InterviewPrep = () => {
@@ -39,7 +40,7 @@ const InterviewPrep = () => {
         setSessionData(response.data.session)
       }
     } catch (error) {
-      console.error(error.message)
+      toast.error(error.message);
     }
 
   }
@@ -70,18 +71,61 @@ const InterviewPrep = () => {
   }, [])
 
   return (
-    <DashboardLayout>
-      <RoleInfoHeader
-        role={sessionData?.role || ""}
-        topicsToFocus={sessionData?.topicsToFocus || ""}
-        experience={sessionData?.experience || ""}
-        questions={sessionData?.questions?.length || "-"}
-        description={sessionData?.description || ""}
-        lastUpdated={
-          sessionData?.updatedAt ? moment(sessionData.updatedAt).format("Do MMM YYYY") : ""
-        }
-      />
-    </DashboardLayout>
+    <>
+      <DashboardLayout>
+        <RoleInfoHeader
+          role={sessionData?.role || ""}
+          topicsToFocus={sessionData?.topicsToFocus || ""}
+          experience={sessionData?.experience || ""}
+          questions={sessionData?.questions?.length || "-"}
+          description={sessionData?.description || ""}
+          lastUpdated={
+            sessionData?.updatedAt ? moment(sessionData.updatedAt).format("Do MMM YYYY") : ""
+          }
+        />
+      </DashboardLayout>
+
+      <div className='container mx-auto pt-4 pb-4 px-4 md:px-0'>
+        <h2 className='text-lg font-semibold text-black'>Interview Q & A</h2>
+        <div className='grid grid-cols-12 gap-4 mt-4 mb-10'>
+          <div className={`col-span-12 ${openLearnMoreDrawer ? "col-span-7" : "col-span-8"}`}>
+            <AnimatePresence>
+              {sessionData?.questions?.map((data, idx) => {
+                return (
+                  <motion.div
+                    key={data._id || idx}
+                    initial={{ opacity: 0, y: -20 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    exit={{ opacity: 0, scale: 0.95 }}
+                    transition={{
+                      duration: 0.4,
+                      type: "spring",
+                      stiffness: 100,
+                      delay: idx * 0.1,
+                      damping: 15,
+                    }}
+                    layout
+                    layoutId={`question-${data._id || idx}`}
+                  >
+                    <>
+                      <QuestionCard
+                        question={data?.question}
+                        answer={data?.answer}
+                        onLearnMore={() => generateConceptExplanation(data.question)}
+                        isPinned={data?.isPinned}
+                        onTogglePin={() => togglePinQuestionsStatus(data._id)}
+                      />
+                    </>
+                  </motion.div>
+                )
+              })}
+            </AnimatePresence>
+          </div>
+        </div>
+      </div>
+
+    </>
+
   )
 }
 
